4a5d68a3b339ff20bb82acb2e9fdbd33aa12477f
xavigisbeg <xavigisbeg@gmail.com>
2016-06-13 11:02:31 +0200

    Can set world up and transform properly. Needs reshaping and take the world flag to a service or merely parameter

--- src/cf_tf_listener.cpp
+++ src/cf_tf_listener.cpp
@@ -1,5 +1,8 @@
 #include <ros/ros.h>
 
+#include <std_msgs/Float64.h>
+#include <vector>
+#include <stdlib.h>
 #include <termios.h>
 #include <stdio.h>
 #include <tf/tf.h>
@@ -13,53 +16,121 @@
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
 
+// Cob_fiducials namespace should be added
 bool flag_world = false;
+geometry_msgs::Pose::_position_type wrldp;
+geometry_msgs::Pose::_orientation_type wrldo;
+
 char getch();
-void Callback(const cob_object_detection_msgs::DetectionArray &msg);
+void Callback(const cob_object_detection_msgs::DetectionArray &msg);        //Do we want reference or not? CppStyleGuide says no
+void broadcastWorld();
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/fiducials/fiducial_detection_array",1,Callback);     // Basic subscriber. Just has the topic it is subscribed to, the callback function and how many messages it caches
-  puts("Press any key to set /world");
+    ros::init(argc, argv, "listener");
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("/fiducials/fiducial_detection_array",10,Callback);     // Basic subscriber. Just has the topic it is subscribed to, the callback function and how many messages it caches
+    puts("Press any key to set /world");
 
-  while(nh.ok()){
-    ros::spinOnce();
-  }
 
-  return 0;
-}
 
-// Performed everytime the /fiducials/fiducial_detection_array is published and then read by this subscribed node
-void Callback(const cob_object_detection_msgs::DetectionArray &msg){                        // const because we are just reading it, & to pass by reference
-    //      WORLD FRAME CREATION      //
+    wrldp.x = 0.0;
+    wrldp.y = 0.0;
+    wrldp.z = 3.0;
+    wrldo.x = 0.0;
+    wrldo.y = 0.0;
+    wrldo.z = 0.0;
+    wrldo.w = 1;
 
-    if (flag_world == false){                                                               // We have no /world yet
-        if (msg.detections.empty() != true){
-            char c = getch();                                                               // Call non-blocking input function
-            if (c == 'w');
-                flag_world = true;                                                          // Will not enter this condition again
-                puts("/world set");
+    while(nh.ok())
+    {
+        ros::spinOnce();
+        try
+        {
+            broadcastWorld();
+            ROS_INFO("World broadcasted");
+            ROS_INFO_STREAM(wrldp.x << " " << wrldo.x);
+        }
+        catch(...)
+        {
+            ROS_INFO("Failed listening cam to marker or broadcasting world");
         }
     }
-    //We have a world
+
+    return 0;
+}
+
+void Callback(const cob_object_detection_msgs::DetectionArray &msg)
+{                        // const because we are just reading it, & to pass by reference
+    //geometry_msgs::Pose::_position_type CFposep[4][3];
+    //geometry_msgs::Pose::_orientation_type CFposeo[4][4];
+    //      WORLD FRAME CREATION      //
+    if (flag_world == false)
+    {                                                               // We have no /world yet
+        if (msg.detections.empty() != true)
+        {
+              // Reads key input in terminal
+
+            char c = getch();                                                               // Call non-blocking input function
+            if (c == 'w');
+            {           // BUG: Does not compare properly the char value, is true when any key is pressed
+
+
+            flag_world = true;                                                              // Will not enter this condition again
+            puts("/world set");
+
+            // Overwrite the global array world with the detected marker's pose
+            wrldp.x = msg.detections[0].pose.pose.position.x;
+            wrldp.y = msg.detections[0].pose.pose.position.y;
+            wrldp.z = msg.detections[0].pose.pose.position.z;
+            wrldo.x = msg.detections[0].pose.pose.orientation.x;
+            wrldo.y = msg.detections[0].pose.pose.orientation.y;
+            wrldo.z = msg.detections[0].pose.pose.orientation.z;
+            wrldo.w = msg.detections[0].pose.pose.orientation.w;
+            }
+        }
+    }
+    // We have a world
     else{
-        if (msg.detections.empty() != true){
-            for (int i=0; i <= msg.detections.size()-1; i++){                               // Checks if a marker has been detected
-                try{
-                    //ROS_INFO_STREAM("Message 1: " << msg.detections[i]);                  // Displays the whole detection information
-                    switch (msg.detections[i].id){                                          // We do this so that we know that we can address the pose to the right marker. Will be replaced
-                        case 0: {ROS_INFO("CF 0:"); break;};
+        if (msg.detections.empty() != true)
+        {
+
+            // We try to post the world marker frame.
+
+            // Here we repeat some actions for each marker detected. Note that marker id and detection [i] are not the same.
+            for (int i=0; i <= msg.detections.size()-1; i++)
+            {                               // Checks if a marker has been detected
+                try
+                {
+                    /*switch (msg.detections[i].id){                                          // We do this so that we know that we can address the pose to the right marker. Will be replaced by the radio choice
+                        case 0: {ROS_INFO("CF 0:"); break;};     //ROS_INFO("CF 0:")
                         case 1: {ROS_INFO("CF 1:"); break;};
                         case 2: {ROS_INFO("CF 2:"); break;};
                         case 3: {ROS_INFO("CF 3:"); break;};
                     default:{ROS_INFO("ID Fail"); break;};                                  // If an error occurs and the detections[i] is accessed erronially
                     }
 
-                    ROS_INFO_STREAM(msg.detections[i].pose.pose);
+                    /*CFposep[msg.detections[i].id][0] = msg.detections[i].pose.pose.position.x;
+                    CFposep[msg.detections[i].id][1] = msg.detections[i].pose.pose.position.y;
+                    CFposep[msg.detections[i].id][2] = msg.detections[i].pose.pose.position.z;
+                    CFposeo[msg.detections[i].id][0] = msg.detections[i].pose.pose.orientation.x;
+                    CFposeo[msg.detections[i].id][1] = msg.detections[i].pose.pose.orientation.y;
+                    CFposeo[msg.detections[i].id][2] = msg.detections[i].pose.pose.orientation.z;
+                    CFposeo[msg.detections[i].id][3] = msg.detections[i].pose.pose.orientation.w;
+
+                    //ROS_INFO_STREAM(msg.detections[i].pose.pose);
+
+                    // We try to post the detected crazyflie's frame
+
+                    tf::TransformBroadcaster brcf;
+                    tf::Transform transformcf[msg.detections.size()];
+                    transformcf[i].setOrigin(tf::Vector3(CFposep[i][0], CFposep[i][1], CFposep[i][2]));
+                    transformcf[i].setRotation(tf::Quaternion(CFposeo[i][0], CFpose[i][1], CFpose[i][2], CFpose[i][3]));
+                    br.sendTransform(tf::StampedTransform(transformcf[i], ros::Time::now(), "world" , "CF" + sprintf(i)));  */
+
                 }
-                catch(...){
+                catch(...)
+                {
                     ROS_INFO("Failed to send Marker "); //sprintf(buffer, i));              // Need to find how to efficiently convert int to string without too much pain
                 }
             }
@@ -68,17 +139,47 @@ void Callback(const cob_object_detection_msgs::DetectionArray &msg){            
     }
 }
 
-// Reads key input in terminal
-char getch(){
-  static struct termios oldt, newt;             // Defines the objects new and old terminals
-  tcgetattr( STDIN_FILENO, &oldt);              // save old settings
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON);                    // disable buffering
-  tcsetattr( STDIN_FILENO, TCSANOW, &newt);     // apply new settings
+void broadcastWorld()
+{
+    //std::isnan();     //Check if the values to pass the transform are valid
+    tf::TransformBroadcaster brw;
+    tf::Transform transformw;
+    tf::TransformListener listenw;
+    ros::Time now = ros::Time::now();
 
-  char c = getchar();                           // read character (non-blocking)
+    // Broadcast the world frame at any moment
 
-  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);     // restore old settings
-  return c;
+    //listenw.waitForTransform("marker", "cam0", now, ros::Duration(10.0));
+    transformw.setOrigin(tf::Vector3(wrldp.x, wrldp.y, wrldp.z));
+    transformw.setRotation(tf::Quaternion(wrldo.x, wrldo.y, wrldo.z, wrldo.w));
+    brw.sendTransform(tf::StampedTransform(transformw.inverse(), ros::Time::now(), "world" , "cam0"));
 }
 
+
+// CODE THAT HAS BEEN TESTED AND IS KEPT FOR FUTURE USEFUL REASONS
+
+/*transformw.setOrigin(tf::Vector3(wrld.detections[0].pose.pose.position.x, wrld.detections[0].pose.pose.position.y, wrld.detections[0].pose.pose.position.z));
+transformw.setRotation(tf::Quaternion(wrld.detections[0].pose.pose.orientation.x, wrld.detections[0].pose.pose.orientation.y, wrld.detections[0].pose.pose.orientation.z, wrld.detections[0].pose.pose.orientation.w));
+brw.sendTransform(tf::StampedTransform(transformw, ros::Time::now(), "world" , "cam0"));*/
+
+
+  // Reads key input in terminal
+char getch()
+{
+    static struct termios oldt, newt;             // Defines the objects new and old terminals
+    tcgetattr( STDIN_FILENO, &oldt);              // save old settings
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON);                    // disable buffering
+    tcsetattr( STDIN_FILENO, TCSANOW, &newt);     // apply new settings
+
+    char c = getchar();                           // read character (non-blocking)
+
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);     // restore old settings
+    return c;
+}
+
+/*char c = getch();                                                               // Call non-blocking input function
+if (c == 'w');
+{           // BUG: Does not compare properly the char value, is true when any key is pressed
+
+}*/
